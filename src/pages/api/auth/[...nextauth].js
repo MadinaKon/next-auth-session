240bfa558b352c7c8f782ec102e569c1710b1a6a
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-16 12:54:14 +0200

    remove unused code

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -37,12 +37,6 @@ export const authOptions = {
       session.user.userId = user.id;
       return session;
     },
-    // async signIn({ account, profile }) {
-    //   if (account.provider === "google") {
-    //     return profile.email_verified && profile.email.endsWith("@example.com");
-    //   }
-    //   return true; // Do different verification for other providers that don't have `email_verified`
-    // },
   },
 };
 
